96c358a3d5b2f1f7d1da3270ba9e2e168fea0e98
PRC <105520102+x0prc@users.noreply.github.com>
2026-01-04 21:17:19 +0530

    fix(engine) : complete algorithms/logic for #1

--- src/android/lib/core/engine/algebraic_engine.dart
+++ src/android/lib/core/engine/algebraic_engine.dart
@@ -49,4 +49,88 @@ class AlgebraicEngine {
     }
     return tokens;
   }
+  static final Map<String, int> _precedence = {
+    '+': 1,
+    '-': 1,
+    '*': 2,
+    '/': 2,
+  };
+
+  static bool _isOp(String c) => '+-*/'.contains(c);
+
+  List<Token> _toRpn(List<Token> tokens) {
+    final output = <Token>[];
+    final ops = <Token>[];
+
+    for (final t in tokens) {
+      switch (t.type) {
+        case TokenType.number:
+          output.add(t);
+          break;
+        case TokenType.op:
+          while (ops.isNotEmpty &&
+              ops.last.type == TokenType.op &&
+              _precedence[ops.last.value]! >= _precedence[t.value]!) {
+            output.add(ops.removeLast());
+          }
+          ops.add(t);
+          break;
+        case TokenType.lParen:
+          ops.add(t);
+          break;
+        case TokenType.rParen:
+          while (ops.isNotEmpty && ops.last.type != TokenType.lParen) {
+            output.add(ops.removeLast());
+          }
+          if (ops.isEmpty || ops.last.type != TokenType.lParen) {
+            throw CalcError.parseError();
+          }
+          ops.removeLast();
+          break;
+      }
+    }
+    while (ops.isNotEmpty) {
+      if (ops.last.type == TokenType.lParen) {
+        throw CalcError.parseError();
+      }
+      output.add(ops.removeLast());
+    }
+    return output;
+  }
+
+  CalcNumber evaluate(String expr) {
+    final tokens = _tokenize(expr);
+    final rpn = _toRpn(tokens);
+    final stack = <CalcNumber>[];
+
+    for (final t in rpn) {
+      if (t.type == TokenType.number) {
+        stack.add(CalcNumber.fromString(t.value));
+      } else if (t.type == TokenType.op) {
+        if (stack.length < 2) throw CalcError.stackUnderflow();
+        final b = stack.removeLast();
+        final a = stack.removeLast();
+        CalcNumber res;
+        switch (t.value) {
+          case '+':
+            res = a + b;
+            break;
+          case '-':
+            res = a - b;
+            break;
+          case '*':
+            res = a * b;
+            break;
+          case '/':
+            res = a / b;
+            break;
+          default:
+            throw CalcError.parseError();
+        }
+        stack.add(res);
+      }
+    }
+    if (stack.length != 1) throw CalcError.parseError();
+    return stack.first;
+  }
 }
--- src/android/lib/core/engine/rpn_engine.dart
+++ src/android/lib/core/engine/rpn_engine.dart
@@ -12,3 +12,86 @@ class RpnStackState {
   }
 }
 
+class RpnEngine {
+  RpnStackState _state = RpnStackState();
+  final List<RpnStackState> _history = [];
+
+  /// Save current state before mutation.
+  void _save() => _history.add(state.copyWith());
+
+  void clear() {
+    _save();
+    state = RpnStackState();
+  }
+
+  void pushNumber(CalcNumber n) {
+    _save();
+    final newStack = List<CalcNumber>.from(state.stack)..add(n);
+    state = state.copyWith(stack: newStack);
+  }
+
+  void applyOp(RpnOp op) {
+    if (state.stack.length < 2) {
+      throw CalcError.stackUnderflow();
+    }
+    _save();
+    final newStack = List<CalcNumber>.from(state.stack);
+    final b = newStack.removeLast();
+    final a = newStack.removeLast();
+    CalcNumber res;
+    switch (op) {
+      case RpnOp.add:
+        res = a + b;
+        break;
+      case RpnOp.sub:
+        res = a - b;
+        break;
+      case RpnOp.mul:
+        res = a * b;
+        break;
+      case RpnOp.div:
+        res = a / b;
+        break;
+    }
+    newStack.add(res);
+    state = state.copyWith(stack: newStack);
+  }
+
+  void undo() {
+    if (_history.isNotEmpty) {
+      state = _history.removeLast();
+    }
+  }
+
+  // Basic stack manipulation
+  void swap() {
+    if (state.stack.length < 2) {
+      throw CalcError.stackUnderflow();
+    }
+    _save();
+    final newStack = List<CalcNumber>.from(state.stack);
+    final temp = newStack.removeLast();
+    newStack.add(newStack.removeLast());
+    newStack.add(temp);
+    state = state.copyWith(stack: newStack);
+  }
+
+  void drop() {
+    if (state.stack.isEmpty) {
+      throw CalcError.stackUnderflow();
+    }
+    _save();
+    final newStack = List<CalcNumber>.from(state.stack)..removeLast();
+    state = state.copyWith(stack: newStack);
+  }
+
+  void dup() {
+    if (state.stack.isEmpty) {
+      throw CalcError.stackUnderflow();
+    }
+    _save();
+    final newStack = List<CalcNumber>.from(state.stack)
+      ..add(state.stack.last);
+    state = state.copyWith(stack: newStack);
+  }
+}
