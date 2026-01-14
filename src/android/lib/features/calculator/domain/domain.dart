6001cffb94d30ebcef7ca61da23797d132cec784
PRC <105520102+x0prc@users.noreply.github.com>
2026-01-14 18:12:35 +0530

    feat(domain): provide arch for operations #2

---
+++ src/android/lib/features/calculator/domain/domain.dart
@@ -0,0 +1,22 @@
+import '../../../core/engine/number.dart';
+import '../../../core/engine/rpn_engine.dart';
+
+/// Collection of specialised operations
+abstract class Domain {
+  String get id;
+  String get name;
+  String get shortLabel;
+  List<DomainOperation> get operations;
+}
+
+/// Operation within a domain
+abstract class DomainOperation {
+  String get id;
+  String get label;
+  String? get description;
+  /// Stack items consumed
+  int get arity;
+
+  /// Throws updated state OR error
+  RpnStackState execute(RpnStackState state);
+}
