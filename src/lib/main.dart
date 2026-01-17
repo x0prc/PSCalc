ad1d9866ee165702a30bf8bf1224561e1d092eff
PRC <105520102+x0prc@users.noreply.github.com>
2026-01-17 23:00:32 +0530

    fix : refactor lib due to android studio #3

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'features/calculator/application/calc_controller.dart';
 import 'features/calculator/domain/basic_domain.dart';
+import 'features/calculator/domain/currency_domain.dart';
 import 'features/calculator/presentation/calc_screen.dart';
 
 void main() {
@@ -14,10 +15,13 @@ class PSCalcApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (_) => CalcController(allDomains: [BasicDomain()]),
+      create: (_) => CalcController(allDomains: [
+        BasicDomain(),
+        CurrencyDomain(),
+      ]),
       child: MaterialApp(
         title: 'PSCalc',
-        theme: ThemeData.dark(useMaterial3: true),
+        theme: ThemeData.dark().copyWith(useMaterial3: true),
         home: const CalcScreen(),
       ),
     );
